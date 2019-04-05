45cd5b8110194533d390221bf4663675ae516021
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-04 22:07:53 -0500

    Add functionality to CLI and Scraper classes.

--- lib/hostel_finder/cli.rb
+++ lib/hostel_finder/cli.rb
@@ -1,7 +1,14 @@
 class HostelFinder::CLI
 
   def call
-    puts "Howdy Y'all!"
+    puts "Welcome! Let's help you find some of the world's best hostels!"
+    # call hostel_type that accepts user input for the type of hostels to look at
+  end
+
+  def hostel_type
+    Scraper.hostel_categories
+    puts "Please enter a number 1-10 to select a category of hostels:"
+    input = gets.strip
   end
 
 end
