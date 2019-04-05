ac1a9b4f64e00e54d90d7e8726694228531e88e5
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-05 10:38:22 -0500

    Make minor changes to CLI and Scraper classes.

--- lib/hostel_finder/cli.rb
+++ lib/hostel_finder/cli.rb
@@ -10,15 +10,18 @@ class HostelFinder::CLI
     input = nil
     while input != "exit"
       # prints a list of the different hostel categories to the user and receives their input
-      HostelFinder::Scraper.hostel_categories.each_with_index do |cat, idx|
+      HostelFinder::Scraper.scrape_categories.each_with_index do |cat, idx|
         puts "#{idx+1}. #{cat}"
       end
       puts "Please enter a number 1-10 to select a category of hostels you would like more info on:"
       input = gets.strip.to_i
 
-      
-
+      # list hostels from selected category - what is best way to do this?
     end
+  end
+
+  def goodbye
+    puts "Goodbye!"
   end
 
 end
--- lib/hostel_finder/scraper.rb
+++ lib/hostel_finder/scraper.rb
@@ -1,10 +1,10 @@
 class HostelFinder::Scraper
 
-  def main_page
+  def self.main_page
     Nokogiri::HTML(open("https://www.hostelworld.com/hoscars"))
   end
 
-  def hostel_categories
+  def self.scrape_categories
     hostel_a = []
     main_page.css("main#content section h3").collect do |x|
       hostel_a << x.text.strip unless x.text.strip == "Best Hostels by Country" || x.text.strip == "Best Hostels by Continent" || x.text.strip == "All other winners" || x.text.strip == "Best Hostel Chain" || x.text.strip == "Best Hostel for Groups"
