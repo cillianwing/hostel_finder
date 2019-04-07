1f072c00ba38e7ce1bdd34be794933a685638202
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-07 11:18:18 -0500

    Update Hostel and Scraper classes such that proper Hostel objects are created after Scraper was refined.

--- lib/hostel_finder/hostel.rb
+++ lib/hostel_finder/hostel.rb
@@ -2,11 +2,10 @@ class HostelFinder::Hostel
   attr_accessor :name, :location, :url, :category, :rating, :qualities # name, location, url, category are the base. Other wills be dependent on me being able to access the hostelworld site.
   @@all = []
 
-  def initialize(name=nil, location=nil, url=nil, category=nil)
+  def initialize(name, location, url)
     @name = name
     @location = location
     @url = url
-    @category = category
     @@all << self
   end
 
@@ -14,13 +13,16 @@ class HostelFinder::Hostel
     self.new(
       ref.css("span.hostel-name-full").text.strip,
       ref.css("div.hostel-address").text.strip,
-      ref.css.attribute("href").value,
-      #need to figure out category
+      ref.css("a.button.hollow").attribute("href").value,
     )
   end
 
   def self.all
     @@all
+  end
+
+  def category(category)
+    @category = category.css("h3").text
   end
 
   def rating
--- lib/hostel_finder/scraper.rb
+++ lib/hostel_finder/scraper.rb
@@ -16,7 +16,7 @@ class HostelFinder::Scraper
   def create_hostels
     # creates Hostel Objects used scraped data
     scrape_categories.each do |hostel|
-      hostel.css("div.hostel-details").each do |info|
+      hostel.css("div.column.column-block").each do |info|
         HostelFinder::Hostel.new_from_categories(info)
         HostelFinder::Hostel.category(hostel)
       end
@@ -28,5 +28,5 @@ end
 
 # hostel_category = x.css("main#content section h3").text
 # hostel_name = x.css("main#content section span.hostel-name-full").text
-# hostel_url = x.css("main#content section div.hostel-details").attribute("href").value
+# hostel_url = x.css("main#content section div.hostel-details a.button.hollow").attribute("href").value
 # hostel_location = x.css("main#content section div.hostel-details div.hostel-address").text
